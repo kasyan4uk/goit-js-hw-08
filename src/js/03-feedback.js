85ecb5e4ac8331d9cecfec1392c0567328121243
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-03-12 14:18:34 +0200

    feedback fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import throttle from 'lodash/throttle';
 const LOCAL_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
-form = document.querySelector(".feedback-form")
+form = document.querySelector('form');
 
 form.addEventListener('input', throttle(storageFormData, 500));
 
@@ -17,6 +17,7 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
+  
   console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(LOCAL_KEY);
