0b09d37bea473f98d4d1e129a985cc3db1cda06a
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-03-12 11:30:22 +0200

    first try

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash/throttle';
+
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(storageFormData, 500));
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+reloadPage();
+
+function reloadPage() {
+  if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
 
